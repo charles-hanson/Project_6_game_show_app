11a2a4aefa6881ac9042f4c9c9d7eb777840c8e3
charles-hanson <51126230+charles-hanson@users.noreply.github.com>
2019-07-15 18:57:20 -0500

    Set variables in app.js

---
+++ app.js
@@ -0,0 +1,6 @@
+let qwerty = document.getElementById('qwerty');
+let phrase = document.getElementById('phrase');
+let missed = 0;
+let btnReset = document.getElementsByClassName('btn__reset');
+let mainContainer = document.getElementsByClassName('main-container');
+let overlay = document.getElementById('overlay');
